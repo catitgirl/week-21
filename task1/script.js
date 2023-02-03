f5f5f16ff42f31a4e61a1da494c71358826c9811
catitgirl <natagan888@gmail.com>
2023-02-03 17:27:07 +0400

    add first task

---
+++ task1/script.js
@@ -0,0 +1,59 @@
+//1. Grab the input value and print it
+
+const getInput = (a) => {
+   let input = document.getElementById("input").value;
+    console.log(input);
+    getName(input);
+};
+
+document.querySelector(".input-search").addEventListener("keyup", (e) => {
+    let input = document.querySelector("input").value;
+    // If the key ENTER is pressed....
+    if (e.which === 13) {
+        getName(input);
+    }
+});
+
+// Interacting with the Giphy API
+const getName = (item) => {
+    const query = item.split(" ").join("+");
+
+    //2. Do the data stuff with API
+    const url =
+        "https://api.giphy.com/v1/gifs/search?q=" +
+        query +
+        "&api_key=b11HdOMgaljN360FcyhSLpDQ4plP203S";
+
+    // AJAX Request
+    const GiphyAJAXCall = new XMLHttpRequest();
+    GiphyAJAXCall.open("GET", url);
+    GiphyAJAXCall.send();
+
+    GiphyAJAXCall.addEventListener("load", (e) => {
+        const data = e.target.response;
+        pushToDom(data);
+    });
+};
+
+//3. Show the GIF's
+const pushToDom = (input) => {
+    const response = JSON.parse(input);
+
+    const container = document.querySelector(".js-container");
+
+    const result = document.querySelector(".results");
+
+
+    const imageUrls = response.data;
+
+    imageUrls.forEach((image) => {
+        const src = image.images.fixed_height.url;
+
+        result.innerHTML = src.length + "gifs found";
+        container.innerHTML += '<img src="' + src + "\" class='container-image'>";
+    });
+
+    const clear = (item) => {
+        item.innerHTML = "";
+    };
+};
